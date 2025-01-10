039951a3b75cc974376a7b119ad43b3a25cff856
Semchenko Oleg <smmmchnk1@gmail.com>
2025-01-10 12:28:22 +0700

    Initial commit

---
+++ internal/storage/migrations/20250110044928_create_articles.sql
@@ -0,0 +1,20 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE articles
+(
+    id           SERIAL PRIMARY KEY,
+    source_id    INT          NOT NULL,
+    title        VARCHAR(255) NOT NULL,
+    link         VARCHAR(255) NOT NULL,
+    summary      TEXT         NOT NULL,
+    published_at TIMESTAMP    NOT NULL,
+    created_at   TIMESTAMP    NOT NULL,
+    posted_at    TIMESTAMP,
+    CONSTRAINT   fk_articles_source_id
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE articles;
+-- +goose StatementEnd
